26a2e97c156c1b9e9bbec866272a44948ded4558
kreideprinzV <160458856+kreideprinzV@users.noreply.github.com>
2024-03-23 20:50:33 +0800

    The client program

---
+++ prac_06/guitars.py
@@ -0,0 +1,29 @@
+from guitar import Guitar
+
+
+def main():
+    print("My guitars!")
+    guitars = []
+
+    # for testing purposes
+    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    # guitars.append(Guitar("Fender Stratocaster", 2014, 765.4))
+    while True:
+        name = input("Name: ")
+        if name == "":
+            break
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        new_guitar = Guitar(name, year, cost)
+        guitars.append(new_guitar)
+        print(f"{new_guitar} added.\n")
+
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth $ {guitar.cost:10,.2f}{vintage_string}")
+
+
+
+main()
