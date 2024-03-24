bee3818e5e1f484e069f36693770d8dc00546ca4
kreideprinzV <160458856+kreideprinzV@users.noreply.github.com>
2024-03-24 21:37:34 +0800

    Language reader

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -22,7 +22,6 @@ class ProgrammingLanguage:
         """Determine if language is dynamically typed."""
         return self.typing == "Dynamic"
 
-
 def run_tests():
     """Run simple tests/demos on ProgrammingLanguage class."""
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
@@ -38,5 +37,5 @@ def run_tests():
             print(language.name)
 
 
-if __name__ == "__main__":
-    run_tests()
+
+run_tests()
